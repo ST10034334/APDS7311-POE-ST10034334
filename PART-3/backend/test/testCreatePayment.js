fd117fac83509f935213d03914fb6bfbd4b27d72
Rachael <ST10034334@vcconnect.edu.za>
2024-11-11 18:14:28 +0200

    Final POE + All Postman + Unit Tests

--- PART-3/backend/test/testCreatePayment.js
+++ PART-3/backend/test/testCreatePayment.js
@@ -17,7 +17,7 @@ async function testCreatePayment() {
       submit_swift: false
     }, {
       headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjZmZDkxN2U4OWIyM2E3OWFkYThhMjcxIiwicm9sZSI6IkN1c3RvbWVyIiwiaWF0IjoxNzMxMzI3NzYxLCJleHAiOjE3MzEzMzEzNjF9.jRuMT5JjhPlLlZHsSGxH7ZtzLWiU7q_l59jL7EoILH4' 
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjZmZTcyNjY0YjA2ZDcyZGI5MWE1NzQwIiwicm9sZSI6IkN1c3RvbWVyIiwiaWF0IjoxNzMxMzQxNTM3LCJleHAiOjE3MzEzNDUxMzd9.eorjhoxM9DMF6d5-hTiGwE0UGEVkek4BiOQ02arZUAQ' 
       }
     });
 
